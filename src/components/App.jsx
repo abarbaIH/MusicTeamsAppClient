11f09630ceab2f1805a9abe0fafceedee0eb9914
Alvaro Barba <abarba.lod@gmail.com>
2023-05-27 14:01:27 +0200

    prueba cliente

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Prueba pagina inicio</h1>
+      <h1>Prueba pagina Lourdes</h1>
+      <h1>Prueba pagina Alvaro</h1>
     </div>
   );
 }
