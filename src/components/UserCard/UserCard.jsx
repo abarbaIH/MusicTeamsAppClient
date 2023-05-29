01794175aaa0c9edde52c868a13d8874941f870c
Alvaro Barba <abarba.lod@gmail.com>
2023-05-29 11:11:56 +0200

    policia de modelos

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -2,10 +2,10 @@ import { Button, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import './UserCard.css'
 
-const UserCard = ({ _id, profileImg, firstName, lastName, aboutMe, instrument }) => {
+const UserCard = ({ _id, avatar, firstName, lastName, aboutMe, instrument }) => {
     return (
         <Card className="mb-3 UserCard" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={profileImg} />
+            <Card.Img variant="top" src={avatar} />
             <Card.Body>
                 <Card.Title>{firstName} {lastName}</Card.Title>
                 <Card.Text>
