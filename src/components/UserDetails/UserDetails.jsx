5a368b9ca2a9799c65cfcab86f3902b1e87dcd1e
LourdesGonzalezT <LourdesGonzalezT@github.com>
2023-06-01 21:09:01 +0200

    day1j

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Row, Col, Form } from "react-bootstrap"
+import { Button, Card, Row, Col, Form, Container } from "react-bootstrap"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import usersService from './../../services/users.services'
 
@@ -21,6 +21,7 @@ const UserDetails = ({ user }) => {
 
         <>
             <h1> {user.firstName}</h1>
+            <hr></hr>
             <Card className="mb-3 UserCard" md={{ span: 6 }} >
                 <Row>
                     <Col>
@@ -53,64 +54,65 @@ const UserDetails = ({ user }) => {
                         <p>Mis eventos creados:{user.firstName}</p>
                         <p>Mis amigos:{user.favorites}</p>
                     </Row> */}
-                    <Card>
+
+                    <Container>
                         <Row>
                             <Col>
-                                <Card.Img style={{ width: '10%', height: '10%' }} variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqResG0Hteqj7GC0T34B3aIf9K2GMyuhq9SA&usqp=CAU" />
-                                <Card.Title>Mis amigos</Card.Title>
-                                {/* <p>{user.favorites.map(f => {
+                                <Card>
+                                    <Card.Img style={{ width: '10%', height: '10%' }} variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqResG0Hteqj7GC0T34B3aIf9K2GMyuhq9SA&usqp=CAU" />
+                                    <Card.Title>Mis amigos</Card.Title>
+                                    {/* <p>{user.favorites.map(f => {
                                     return (
                                         <li key={f}>
                                             {f}
                                         </li>
                                     )
                                 })}</p> */}
-
+                                </Card>
                             </Col>
                             <Col>
-                                <Card.Img style={{ width: '10%', height: '10%' }} variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqResG0Hteqj7GC0T34B3aIf9K2GMyuhq9SA&usqp=CAU" />
-                                <Card.Title>Mis ensayos</Card.Title>
-                                {/* <p>{user.eventsAssisted.map(ev => {
+                                <Card>
+                                    <Card.Img style={{ width: '10%', height: '10%' }} variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqResG0Hteqj7GC0T34B3aIf9K2GMyuhq9SA&usqp=CAU" />
+                                    <Card.Title>Mis ensayos</Card.Title>
+                                    {/* <p>{user.eventsAssisted.map(ev => {
                                     return (
                                         <li key={ev}>
                                             {ev}
                                         </li>
                                     )
                                 })}</p> */}
-
+                                </Card>
                             </Col>
                             <Col>
-                                <Card.Img style={{ width: '10%', height: '10%' }} variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqResG0Hteqj7GC0T34B3aIf9K2GMyuhq9SA&usqp=CAU" />
-                                <Card.Title>Mis salas favoritas</Card.Title>
-                                {/* <p>{user.venueFavorites.map(v => {
+                                <Card>
+                                    <Card.Img style={{ width: '10%', height: '10%' }} variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqResG0Hteqj7GC0T34B3aIf9K2GMyuhq9SA&usqp=CAU" />
+                                    <Card.Title>Mis salas favoritas</Card.Title>
+                                    {/* <p>{user.venueFavorites.map(v => {
                                     return (
                                         <li key={v}>
                                             {v}
                                         </li>
                                     )
                                 })}</p> */}
+                                </Card>
                             </Col>
-                            <Button as="span" variant="dark">
-                                <Link to={`/perfil-editar/${user._id}`}>Editar</Link>
-                            </Button>
                         </Row>
-                    </Card>
 
-
-                    <div className="d-grid">
-                        <Button as="span" variant="dark">
+                    </Container>
+                    <Row>
+                        <Col><Button as="span" variant="dark">
                             <Link to={`/perfil-editar/${user._id}`}>Editar</Link>
                         </Button>
-                        <Form onSubmit={handleSubmit}>
+                        </Col>
+                        <Col><Form onSubmit={handleSubmit}>
                             <Button variant="danger" type="submit">Eliminar perfil</Button>
                         </Form>
-                        {/* además botón para el admin cambiar role */}
-                        <Button variant="primary">Cambiar rol</Button>
-
-                    </div>
-
-
-
+                        </Col>
+                        <Col>
+                            {/* además botón para el admin cambiar role */}
+                            <Button variant="dark">Cambiar rol</Button>
+                        </Col>
+                    </Row>
                 </Card>
             </Card >
         </>
